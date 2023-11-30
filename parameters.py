f7b2633ab36bbf06a0377859beac19fc49add376
MadCavarly <147071482+MadCavarly@users.noreply.github.com>
2023-11-30 14:12:52 +0100

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,4 @@
+class Parameters:
+    def __init__(self, check_time=1800, soil_moisture_limit=35, after_control=False):
+        self.check_time = check_time
+        self.soil_moisture_limit = soil_moisture_limit
